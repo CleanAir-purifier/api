37e52cde4b21988c12e7ad072e27171302bbe75b
Sara Silva <saracsas2@gmail.com>
2021-05-12 00:32:24 -0300

    Updata mobile_sensor query

--- app/controllers/mobile_sensors.py
+++ app/controllers/mobile_sensors.py
@@ -1,8 +1,17 @@
+from pydantic import BaseModel
+from typing import Optional
+
 from app.settings import db
 
 
+class UpdateMobileSensor(BaseModel):
+    name: Optional[str] = None
+
+
 def get_mobile_sensor_by_id(mobile_sensor_id: int):
-    mobile_sensor_data = db.clean_air.find_one(
-        {"device.id": mobile_sensor_id, "device.type": "mobile_sensor"})
-    if mobile_sensor_data:
-        return mobile_sensor_data['device']
+    purifier_data = db.clean_air.find_one(    
+        {"mobile_sensors.id": mobile_sensor_id}
+    )
+    if purifier_data:    
+        mobile_sensor = [e for e in purifier_data["mobile_sensors"] if e["id"] == mobile_sensor_id]
+        return mobile_sensor[0]
