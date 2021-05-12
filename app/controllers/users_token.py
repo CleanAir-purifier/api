f0968e22822f7f0359a5482af1faf54cadfb8d75
Rossicler Júnior <rossiclerjr@gmail.com>
2021-05-12 14:21:06 -0300

    Added endpoint to save a user token linked to a purifier

---
+++ app/controllers/users_token.py
@@ -0,0 +1,14 @@
+from app.settings import db
+from pydantic import BaseModel
+
+
+class UserToken(BaseModel):
+    purifier_id: int = 0
+    user_token: str = True
+
+
+def save_user_token(user_token):
+    purifier_data = db.users_token.insert_one(user_token.dict())
+    if purifier_data:
+        return "User token saved."
+    return "Failed to save token."
