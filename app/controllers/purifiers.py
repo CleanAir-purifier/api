cd6115ed65078e3dc9f4eb6d190854d2aee1c193
Sara Silva <saracsas2@gmail.com>
2021-05-12 00:21:45 -0300

    Update purifier query

--- app/controllers/purifiers.py
+++ app/controllers/purifiers.py
@@ -3,6 +3,7 @@ from app.settings import db
 
 def get_purifier_by_id(purifier_id: int):
     purifier_data = db.clean_air.find_one(
-        {"device.id": purifier_id, "device.type": "purifier"})
+        {"_id":purifier_id}
+    )
     if purifier_data:
-        return purifier_data['device']
+        return purifier_data
